2d2d76878a03b61ee76ee5b453c9e5217e1ebf8a
Facundo Lopez <flopezpistan@gmail.com>
2024-06-28 00:11:54 -0300

    Funcion de Registro, login y logout

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import authenticate, login as django_login
+from usuarios.forms import RegisterForm
 
 
 def login(request):
@@ -18,3 +19,16 @@ def login(request):
     
     
     return render(request, "usuarios/login.html", {"formulario": formulario})
+
+def register(request):
+    
+    formulario = RegisterForm()
+    
+    if request.method == "POST":
+        formulario = RegisterForm(request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            return redirect("login")
+            
+    
+    return render(request, "usuarios/register.html", {"formulario": formulario})
